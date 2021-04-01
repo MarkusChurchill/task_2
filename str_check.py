e2570cd5272d713a6cc7f22bdbc07cd101ae4a03
Марат Шагиев <80579316+MarkusChurchill@users.noreply.github.com>
2021-04-01 19:37:59 +0500

    Update str_check.py
    Отвратительная хуйня, писали с димоном вместе, но вроде работает

--- str_check.py
+++ str_check.py
@@ -1,37 +1,62 @@
-#крч нихуя не разобрался с индексами списками и кортежах, начну читать всё заново.
-#Прога ниже может проверить экспрешн на скобки ( в том числе вложенность скобок) но не более. 
-def checkio(expression):
-    list_brackets = []
-    for i in expression:
-        if i == "[" or i == "]" or i == "(" or i == ")" or i == "{" or i == "}":
-            list_brackets.append(i) #создаю список нужных нам элементов
-    if list_brackets.count("[") == list_brackets.count("]") and list_brackets.count("(") == list_brackets.count(")") and list_brackets.count("{") == list_brackets.count("}"):
-      #если количество "(" равно количеству ")" и так далее...
-        j = 0
-        k = 0
-        try:
-            while j != range(len(list_brackets)): #попытка найти в списке соседние скобки
-                if list_brackets[j] == "(" and list_brackets[j+1] == ")":
-                    list_brackets.pop(j)
-                    list_brackets.pop(j)
-                    j = 0
-                elif list_brackets[j] == "{" and list_brackets[j+1] == "}":
-                    list_brackets.pop(j)
-                    list_brackets.pop(j)
-                    j = 0
-                elif list_brackets[j] == "[" and list_brackets[j+1] == "]":
-                    list_brackets.pop(j)
-                    list_brackets.pop(j)
-                    j = 0
-                else:
-                    j += 1
-
-        except:
-           if (len(list_brackets)) == 0:
-               print(True,None,None)
-           else:
-                print(False)
-
+s=input()
+form=list(s)
+stack=[]
+skobki=input()
+close_skobki=[]
+index=[]
+close_index=[]
+close_stack=[]
+a=0
+c=0
+b=len(form)
+d=len(form)
+oshibka_open=None
+oshibka_close=None
+if '(' in skobki:
+    close_skobki.append(')')
+if '[' in skobki:
+    close_skobki.append(']')
+if '{' in skobki:
+    close_skobki.append('}')
+if '<' in skobki:
+    close_skobki.append('>')
+#Проверка на то, что введенные скобки это скобки
+status=True
+for x in skobki:
+    if '(' in x or '[' in x or '{' in x or '<' in x:
+        break
     else:
-            print(False)
-checkio(input())
+        quit()
+#Проверка на наличие скобок в тексте
+for i in s:
+    #Если открывающаяся скобка в тексте
+    if i in list(skobki):
+        stack.append(i) #Добавление открывающейся скобки в stack
+        index.append(form.index(i,a,b)) #Добавление скобки в index
+        a=form.index(i,a,b)+1 #Добавление индекса скобки в a
+
+    elif i in close_skobki: #Если закрывающаяся скобка в тексте
+        close_stack.append(i) #Добавление закрывающейся скобки в close_stack
+        close_index.append(form.index(i, c, d)) #Добавление закрывающейся скобки в close_stack
+        c = form.index(i, c, d) + 1
+        if not stack:
+            status=False
+            break
+        open_bracket=stack.pop()
+        open_index = index.pop()
+        if open_bracket in skobki and open_bracket=='(' and i==')':
+            continue
+        if open_bracket in skobki and open_bracket=='[' and i==']':
+            continue
+        if open_bracket in skobki and open_bracket=='{' and i=='}':
+            continue
+        if open_bracket in skobki and open_bracket=='<' and i=='>':
+            continue
+        status=False
+        break
+if status and len(stack)==0 or status and skobki not in open_bracket:
+    print(status, oshibka_open, oshibka_close)
+else:
+    oshibka_open=(open_bracket, open_index)
+    oshibka_close=(close_index[0], close_stack[0])
+    print(status, oshibka_open, oshibka_close)
